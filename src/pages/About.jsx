25abd3bc4991bcd1923a6d888dc98a948f896b03
administrateur <administrateur@FVFCH0QUL410.local>
2026-03-10 11:56:10 +0100

    Ajout du 3D dans A propos

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <h1>Page about de mon Portfolio</h1>;
+import Spline from '@splinetool/react-spline';
+
+export default function About() {
+  return (
+    <div className="about-container">
+      <h1>À propos de moi</h1>
+      <p>Bienvenue dans mon univers interactif.</p>
+      
+      {/* Le conteneur du présentoir 3D */}
+      <div className="spline-wrapper">
+        <Spline scene="https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode" />
+      </div>
+    </div>
+  );
 }
